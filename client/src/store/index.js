4fa7b3a18d5f7c268d6386d4ad1c7e5590b21233
Natthapong Pecpipat <102534597+XspOttEr15@users.noreply.github.com>
2023-07-22 23:23:55 +0700

    first commit

---
+++ client/src/store/index.js
@@ -0,0 +1,14 @@
+import { proxy } from "valtio";
+
+const state = proxy({
+
+    intro: true,
+    color: '#7248ef',
+    isLogoTexture: true,
+    isFullTexture: false,
+    logoDecal: './threejs.png',
+    fullDecal: './threejs.png',
+    
+});
+
+export default state;
